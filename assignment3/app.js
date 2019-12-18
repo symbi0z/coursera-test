35f3baa821cc7f1dbebceb2f0464d8cb3104afa5
symbi0z <58937850+symbi0z@users.noreply.github.com>
2019-12-18 14:06:19 +0100

    Add files via upload

--- assignment3/app.js
+++ assignment3/app.js
@@ -58,19 +58,30 @@ function MenuSearchService($q) {
   var service = this;
 
   service.getMatchedMenuItems = function (searchTerm) {
-    var promise = $http({
-      method: "GET",
-      url: (ApiBasePath + "/menu_items.json")
-    });
+    // var promise = $http({
+    //   method: "GET",
+    //   url: (ApiBasePath + "/menu_items.json")
+    // });
+    //
+    //  return promise.then(function (response) {
+    //   console.log(response.data);
+    data = [
+      {id:877,short_name:"A1",name:"Won Ton Soup with Chicken",description:"chicken-stuffed won tons in clear chicken broth with white meat chicken pieces and a few scallions",price_small:2.55,price_large:5.0,small_portion_name:"pint",large_portion_name:"quart"}
+      ,{id:878,short_name:"A2",name:"Egg Drop Soup",description:"chicken broth with egg drop",price_small:2.25,price_large:4.5,small_portion_name:"pint",large_portion_name:"quart"},{id:879,short_name:"A3",name:"Chicken Corn Soup",description:"clear chicken broth with creamy corn and egg drop with white meat chicken pieces",price_small:2.75,price_large:5.5,small_portion_name:"pint",large_portion_name:"quart"},{id:880,short_name:"A4",name:"Hot and Sour Soup",description:"tofu, chicken, mushroom, bamboo shoot, and egg",price_small:2.55,price_large:5.0,small_portion_name:"pint",large_portion_name:"quart"},{id:881,short_name:"A5",name:"Egg Drop with Won Ton Soup",description:"chicken soup with egg drop and won tons",price_small:3.0,price_large:6.0,small_portion_name:"pint",large_portion_name:"quart"},{id:882,short_name:"A6",name:"Chicken Noodle (or Rice) Soup",description:"clear broth and lo mein noodles or white rice, chicken pieces",price_small:2.55,price_large:5.0,small_portion_name:"pint",large_portion_name:"quart"},{id:883,short_name:"A7",name:"Garden Vegetable Soup",description:"clear chicken broth with mixed vegetables (carrots, cabbage, baby corn, mushroom, snow peas)",price_small:2.55,price_large:5.0,small_portion_name:"pint",large_portion_name:"quart"},{id:884,short_name:"A8",name:"Garden Vegetable Soup with Tofu",description:"clear chicken broth with mixed vegetables (carrots, cabbage, baby corn, mushroom, snow peas) with tofu pieces",price_small:3.0,price_large:6.0,small_portion_name:"pint",large_portion_name:"quart"}
+    ];
 
-     return promise.then(function (response) {
-      console.log(response.data);
        // process result and only keep items that match
       var foundItems=[];
 
+      for (var i = 0; i < data.length; i++) {
+        if(data[i] != null && data[i].description != null && data[i].description.indexOf(searchTerm) !== -1) {
+          foundItems.push(data[i]);
+        }
+      }
+
       // return processed items
       return foundItems;
-    });
+  //  });
   };
 }
 
